899ed51dbc2eaed044c8a5ee89b07096627c3f87
Pellew <68203188+Pellew@users.noreply.github.com>
2026-02-09 14:01:23 +0100

    Add files via upload

--- MoneyFlow.lua
+++ MoneyFlow.lua
@@ -1,5 +1,5 @@
 --==========================================================
--- Money Flow - Version 2.0.5a
+-- Money Flow - Version 2.0.6
 --==========================================================
 -- Highlights:
 --  - NO bag hook install on login (lazy install when bags are toggled)
